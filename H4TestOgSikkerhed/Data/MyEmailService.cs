355849a305e76fd85b1762887cd086a0ed3628c6
Zeritar <mm_9290@hotmail.com>
2023-01-17 12:52:14 +0100

    Hele lortet til master

---
+++ H4TestOgSikkerhed/Data/MyEmailService.cs
@@ -0,0 +1,13 @@
+﻿using NuGet.Common;
+
+namespace H4TestOgSikkerhed.Data
+{
+    public static class MyEmailService
+    {
+        public static void SendTwoFactorCode(string code)
+        {
+            // Gem token kode i en tekst fil som test
+            File.WriteAllText("token.txt", code);
+        }
+    }
+}
