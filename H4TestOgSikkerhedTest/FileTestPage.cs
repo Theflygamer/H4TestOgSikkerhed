84a8f0f59e0be89c9e8fbaa9a1e06ced12d1f2ca
Theflyamer <Frederikteddy01@gmail.com>
2023-01-19 10:53:20 +0100

    button

---
+++ H4TestOgSikkerhedTest/FileTestPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4TestOgSikkerhedTest
+{
+    public class FileTestPage
+    {
+        [Fact]
+        public void Testfile()
+        {
+            using (var webdriver = new ChromeDriver())
+            {
+                webdriver.Navigate().GoToUrl("https://localhost:7084/filetest");
+
+                var button = webdriver.FindElement(By.Id("filetest_button"));
+                button.Click();
+            }
+        }
+
+
+
+
+
+    }
+}
